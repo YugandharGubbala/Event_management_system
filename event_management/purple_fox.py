db4b8655ef711fdbe6a16bae89be7d5074ea0f7e
pavithran-mm <pavithran.mm@thoughtworks.com>
2024-07-04 15:24:11 +0530

    [Pavithran] Add. event, service, city selection feature

--- event_management/purple_fox.py
+++ event_management/purple_fox.py
@@ -3,8 +3,28 @@ from event_management.services.user_registration import UserRegistration
 
 class PurpleFox:
     def __init__(self):
+        self.selected_city = None
+        self.occasion_type = None
         self.user_registration = UserRegistration()
-        self.selectedServices = {}
+        self.selected_services = set()
 
     def register_user(self, username, password):
         return self.user_registration.register(username, password)
+
+    def select_occasion_type(self, occasion_type):
+        self.occasion_type = occasion_type
+
+    def get_occasion_type(self):
+        return self.occasion_type
+
+    def select_city_for_venue(self, city):
+        self.selected_city = city
+
+    def get_selected_city(self):
+        return self.selected_city
+
+    def select_service(self, service):
+        self.selected_services.add(service)
+
+    def get_selected_service(self):
+        return self.selected_services
--- tests/test_purple_fox.py
+++ tests/test_purple_fox.py
@@ -1,13 +1,43 @@
 import unittest
 
+from event_management.enums.city import City
+from event_management.enums.event_service import EventService
+from event_management.enums.occasion_type import OccasionType
 from event_management.purple_fox import PurpleFox
 
 
 class TestPurpleFox(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.purple_fox = PurpleFox()
 
     def test_user_registration_return_true_if_user_is_registered(self):
-        purple_fox = PurpleFox()
-
-        is_user_registered = purple_fox.register_user("Aditya", "Aditya369")
+        is_user_registered = self.purple_fox.register_user("Aditya", "Aditya369")
 
         self.assertTrue(is_user_registered)
+
+    def test_select_occasion_type(self):
+        self.purple_fox.select_occasion_type(OccasionType.WEDDING)
+
+        occasion_type = self.purple_fox.get_occasion_type()
+
+        self.assertEqual(OccasionType.WEDDING, occasion_type)
+
+    def test_selected_city_for_venue(self):
+        self.purple_fox.select_city_for_venue(City.COIMBATORE)
+
+        selected_city = self.purple_fox.get_selected_city()
+
+        self.assertEqual(City.COIMBATORE, selected_city)
+
+    def test_select_service(self):
+        self.purple_fox.select_service(EventService.PHOTOGRAPHY)
+
+        selected_services = self.purple_fox.get_selected_service()
+
+        self.assertEqual({EventService.PHOTOGRAPHY}, selected_services)
+
+        self.purple_fox.select_service(EventService.FOOD_CATERING)
+        selected_services = self.purple_fox.get_selected_service()
+
+        self.assertEqual({EventService.PHOTOGRAPHY, EventService.FOOD_CATERING}, selected_services)
